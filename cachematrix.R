da58350cf67d3422b8586b04eed22abaa1ba6a86
shukeh <shubham.rakesh@yahoo.com>
2016-12-03 09:15:46 +0530

    Add files via upload
    The R script for programming-assignment-2-lexical-scoping

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,50 @@
-## Put comments here that give an overall description of what your
-## functions do
+# The makeCacheMatrix function creates a list containing set of functions to
+# 1. set the value of the matrix
+# 2. get the value of the matrix
+# 3. set the value of inverse of the matrix
+# 4. get the value of inverse of the matrix
 
-## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  
+  set <- function(y){
+    x <<- y
+    inv <<- NULL
+  }
+  
+  get <- function() {
+    x
+  }
+  
+  setinvmatrix <- function(inverse) {
+    inv <<-  inverse
+  }
+  
+  getinvmatrix <-function(){
+    inv  
+  }
+  
+  list(set=set,get=get,setinvmatrix=setinvmatrix,getinvmatrix=getinvmatrix)
 }
 
 
-## Write a short comment describing this function
+# The cacheSolve() function returns the inverse of the matrix.If the inverse of the matrix 
+# is already available then it gets the result and skips the
+# computation. If not, it computes the inverse, sets the value in the cache using
+# setinverse function.
+
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inv <- x$getinvmatrix()
+  
+  if(!is.null(inv)){
+    message("Reading data from cache")
+    return(inv)
+  }
+  
+  inv <- solve(x$get())
+  x$setinvmatrix(inv)
+  inv
+  
 }
